c943334f497ee3549f01e94ff4e6f2dffd9bd6f2
Simon Frid <simon@simonfrid.com>
2023-11-14 17:08:08 -0700

    Update asttokens req in setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     python_requires='>=3.6',
     py_modules=['vyper'],
     install_requires=[
-        'asttokens==2.0.3',
+        'asttokens>=2.0.3',
         'pycryptodome>=3.5.1,<4',
     ],
     setup_requires=[
